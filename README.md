95e120921e693c11dbccbc8c35602b7e4e89298d
Eric Arellano <ericarellano@me.com>
2017-11-12 14:18:32 -0700

    change to venv

--- README.md
+++ README.md
@@ -5,11 +5,34 @@ Simple app to expose students' service hours through Google Docs API.
 
 ### Requirements
 1. Python 3.6
-1. VirtualEnvWrapper - use with Python to keep your dependencies just for this project
-1. Node 8.1 - download NVM (Node Version Manager) and use NVM to use this version of Node.js, the most recent and stable.
-1. Yarn - this is a build tool for JavaScript. Download it with NPM after installing NVM and Node
+    1. With Mac, use HomeBrew and `brew install python3`
+1. NVM (Node Version Manager) 
+1. Node.js (use NVM)
+    1. `nvm install 8.1.4`
+    1. `nvm use 8.1.4`
+1. Yarn
+    1. `npm install -g yarn`
 
-## To start
+#### Backend
+1. `cd backend/`
+1. Make a virtual environment
+    1. On Mac, `python3 -m venv ./`
+    1. On Windows, `c:\Python35\python -m venv ./` 
+1. `pip install -r requirements.txt`
+
+#### Frontend
 1. `cd frontend/`
 1. `yarn install`
+
+## To start
+
+#### Backend
+1. `cd backend/`
+1. activate virtual environment
+    1. On Mac, `source bin/activate`
+    1. On Windows, `\Scripts\activate.bat`
+1. `flask run`
+
+#### Frontend
+1. `cd frontend/`
 1. `yarn start`
--- backend/app.py
+++ backend/app.py
@@ -4,10 +4,11 @@ import flask
 import json
 
 from apiclient import discovery
-from flask import render_template
 from httplib2 import Http
 from oauth2client import tools
 from oauth2client.service_account import ServiceAccountCredentials
+
+app = flask.Flask(__name__)
 
 scopes = ['https://www.googleapis.com/auth/sqlservice.admin', 'https://www.googleapis.com/auth/spreadsheets.readonly']
 
@@ -17,12 +18,14 @@ try:
 except ImportError:
     flags = None
 
+
 class Event:
     def __init__(self, name, type, hours, status):
         self.name = name
         self.type = type
         self.hours = hours
         self.status = status
+
 
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/sheets.googleapis.com-python-quickstart.json
@@ -33,22 +36,18 @@ APPLICATION_NAME = 'Google Sheets API Python Quickstart'
 MASTER_2017_ID = '1H5leinJFGT1SDfb2hqbDpQgSC_2GYr1HFwKPpzFZ1Js'
 ENGAGEMENT_2017_ID = '1FBDR19w831QQ8XTFMns6qVzGEGTv1UU7NAoomTaJRLA'
 
-app = flask.Flask(__name__)
 
 @app.route('/test')
 def test_api_request():
     print("Testing testing")
     return True
 
-@app.errorhandler(404)
-def not_found(error):
-    return render_template('404.html'), 404
 
 # @app.route('/api/engagement/<int:student_id>')
 # def get_engagement(student_id):
 
 
-@app.route('/api/student_id/<int:id>')
+@app.route('/api/student_info/<int:id>')
 def get_name(id):
     credentials = get_credentials()
     http_auth = credentials.authorize(Http())
@@ -63,36 +62,14 @@ def get_name(id):
     else:
         return json.dumps(result), 200
 
+
 def get_credentials():
-    """Gets valid user credentials from storage.
-
-    If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth2 flow is completed to obtain the new credentials.
-
-    Returns:
-        Credentials, the obtained credential.
-    """
     current_dir = os.getcwd()
     credential_path = os.path.join(current_dir, 'service_key.json')
     credentials = ServiceAccountCredentials.from_json_keyfile_name(
         credential_path, scopes)
-    # credential_dir = os.path.join(home_dir, '.credentials')
-    # if not os.path.exists(credential_dir):
-    #     os.makedirs(credential_dir)
-    # credential_path = os.path.join(credential_dir,
-    #                                'sheets.googleapis.com-python-quickstart.json')
-    #
-    # store = Storage(credential_path)
-    # credentials = store.get()
-    # if not credentials or credentials.invalid:
-    #     flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-    #     flow.user_agent = APPLICATION_NAME
-    #     if flags:
-    #         credentials = tools.run_flow(flow, store, flags)
-    #     else: # Needed only for compatibility with Python 2.6
-    #         credentials = tools.run(flow, store)
-    #     print('Storing credentials to ' + credential_path)
     return credentials
+
 
 def get_names(service):
     spreadsheet_id = MASTER_2017_ID
@@ -101,12 +78,14 @@ def get_names(service):
         spreadsheetId=spreadsheet_id, range=range_name).execute()
     return result.get('values', [])
 
+
 def get_name_by_id(service, id):
     results = get_names(service)
     for row in results:
         if row[2] == id:
-            return {'id': id, 'name' : {'first' : row[1], 'last' : row[2] }}
+            return {'id': id, 'name': {'first': row[1], 'last': row[2]}}
     return None
+
 
 def get_events(service):
     spreadsheet_id = ENGAGEMENT_2017_ID
@@ -115,12 +94,14 @@ def get_events(service):
         spreadsheet_id=spreadsheet_id, range=range_name).execute()
     return result.get('values', [])
 
+
 def get_approved_events(service):
     spreadsheet_id = ENGAGEMENT_2017_ID
     range_name = 'Requirements!A2:C'
     result = service.spreadsheets().values().get(
         spreadsheet_id=spreadsheet_id, range=range_name).execute()
     return result.get('values', [])
+
 
 def get_events_by_id(service, id):
     events = get_events(service)
@@ -158,34 +139,28 @@ def get_events_by_id(service, id):
     elif len(all_events) == 0:
         return None
     else:
-        return {"id" : id, "approvedService" : accepted_service_hours, "approvedCivilMil" : accepted_civil_mil, "requirements": event_dict}
+        return {"id": id, "approvedService": accepted_service_hours, "approvedCivilMil": accepted_civil_mil,
+                "requirements": event_dict}
 
-
-def main():
-    """Shows basic usage of the Sheets API.
-
-    Creates a Sheets API service object and prints the names and majors of
-    students in a sample spreadsheet:
-    https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
-    """
-    credentials = get_credentials()
-    # delegated_credentials = credentials.create_delegated('dkchen1@asu.edu')
-    http_auth = credentials.authorize(Http())
-    discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
-                    'version=v4')
-    service = discovery.build('sheets', 'v4', http=http_auth,
-                              discoveryServiceUrl=discoveryUrl)
-    values = get_names(service)
-
-    if not values:
-        print('No data found.')
-    else:
-        print('Last Name, First Name, Student ID:')
-        for row in values:
-            # Print columns A and E, which correspond to indices 0 and 4.
-            print('%s, %s, %s' % (row[0], row[1], row[2]))
-
-
-if __name__ == '__main__':
-    main()
-
+#
+# def main():
+#     credentials = get_credentials()
+#     # delegated_credentials = credentials.create_delegated('dkchen1@asu.edu')
+#     http_auth = credentials.authorize(Http())
+#     discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
+#                     'version=v4')
+#     service = discovery.build('sheets', 'v4', http=http_auth,
+#                               discoveryServiceUrl=discoveryUrl)
+#     values = get_names(service)
+#
+#     if not values:
+#         print('No data found.')
+#     else:
+#         print('Last Name, First Name, Student ID:')
+#         for row in values:
+#             # Print columns A and E, which correspond to indices 0 and 4.
+#             print('%s, %s, %s' % (row[0], row[1], row[2]))
+#
+#
+# if __name__ == '__main__':
+#     main()
