f40b555f43fa70c3c32c1182d69711d69cdbb741
Diana <crystaljerry@gmail.com>
2017-11-12 09:49:55 -0700

    Add service account

--- backend/app.py
+++ backend/app.py
@@ -6,9 +6,13 @@ import requests
 
 from apiclient import discovery
 from flask import render_template
+from httplib2 import Http
 from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
+from oauth2client.service_account import ServiceAccountCredentials
+
+scopes = ['https://www.googleapis.com/auth/sqlservice.admin', 'https://www.googleapis.com/auth/spreadsheets.readonly']
 
 try:
     import argparse
@@ -67,23 +71,26 @@ def get_credentials():
     Returns:
         Credentials, the obtained credential.
     """
-    home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir, '.credentials')
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,
-                                   'sheets.googleapis.com-python-quickstart.json')
-
-    store = Storage(credential_path)
-    credentials = store.get()
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-        flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else: # Needed only for compatibility with Python 2.6
-            credentials = tools.run(flow, store)
-        print('Storing credentials to ' + credential_path)
+    current_dir = os.getcwd()
+    credential_path = os.path.join(current_dir, 'service_key.json')
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(
+        credential_path, scopes)
+    # credential_dir = os.path.join(home_dir, '.credentials')
+    # if not os.path.exists(credential_dir):
+    #     os.makedirs(credential_dir)
+    # credential_path = os.path.join(credential_dir,
+    #                                'sheets.googleapis.com-python-quickstart.json')
+    #
+    # store = Storage(credential_path)
+    # credentials = store.get()
+    # if not credentials or credentials.invalid:
+    #     flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+    #     flow.user_agent = APPLICATION_NAME
+    #     if flags:
+    #         credentials = tools.run_flow(flow, store, flags)
+    #     else: # Needed only for compatibility with Python 2.6
+    #         credentials = tools.run(flow, store)
+    #     print('Storing credentials to ' + credential_path)
     return credentials
 
 def main():
@@ -94,10 +101,11 @@ def main():
     https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
     """
     credentials = get_credentials()
-    http = credentials.authorize(httplib2.Http())
+    delegated_credentials = credentials.create_delegated('dkchen1@asu.edu')
+    http_auth = credentials.authorize(Http())
     discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
                     'version=v4')
-    service = discovery.build('sheets', 'v4', http=http,
+    service = discovery.build('sheets', 'v4', http=http_auth,
                               discoveryServiceUrl=discoveryUrl)
 
     spreadsheetId = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
