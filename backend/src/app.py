9d08e618d759fb9c9f03b818c64ecdf5fe348b55
Eric Arellano <ericarellano@me.com>
2017-11-23 13:06:32 -0800

    git app to run through flask locally!

--- backend/src/app.py
+++ backend/src/app.py
@@ -4,9 +4,14 @@ import json
 
 from .queries import get_demographics, get_engagement
 
-app = flask.Flask(__name__)
+app = flask.Flask(__name__,
+                  static_folder="../../frontend/build/static",
+                  template_folder="../../frontend/build")
 app.url_map.strict_slashes = False
 
+@app.route('/')
+def render_react():
+    return flask.send_from_directory('../../frontend/build', 'index.html')
 
 @app.route('/api/test')
 def api_test():
