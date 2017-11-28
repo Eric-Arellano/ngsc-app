8cf60fc95e35e3ea05abb136e17ea79d50150d9c
Eric Arellano <ericarellano@me.com>
2017-11-27 18:15:42 -0700

    de-duplicate api calls

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -3,13 +3,15 @@ import axios from 'axios'
 
 export const getDemographics = (id: number) => {
   const api = `/api/demographics/${id}`
-  return axios.get(api)
-    .then(info => info.data)
-    .catch(error => error.status)
+  return getRequest(api)
 }
 
 export const getRequirements = (id: number) => {
   const api = `/api/engagement/${id}`
+  return getRequest(api)
+}
+
+const getRequest = (api: string) => {
   return axios.get(api)
     .then(info => info.data)
     .catch(error => error.status)
