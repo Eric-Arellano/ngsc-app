eea732a60107fd283efdfd52f753db333e6427d6
wang-albert <34350148+wang-albert@users.noreply.github.com>
2017-12-23 00:21:25 -0800

    Update Attendance.js

--- frontend/src/components/EngagementView/Attendance.js
+++ frontend/src/components/EngagementView/Attendance.js
@@ -11,8 +11,10 @@ const Attendance = (props: Props) => {
     return (
         <Panel header='Attendance'>
             <Entry>No-shows: {props.noShows}</Entry>
+            <Entry>Mission Team Percent: {props.mt_percent}</Entry>
+            <Entry>Committee Percent: {props.com_percent}</Entry>
             <p><em>
-                * No-shows indicate the number of times that you have RSVP'd that you will be attending an event but did
+                * No-shows indicate the number of times that you have RSVPed that you will be attending an event but did
                 not sign in on our records. If you need to dispute this number, please let e-mail the Admin chair.
             </em></p>
         </Panel>
