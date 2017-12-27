40bbdd688a27f5dca5771b3e5422e522b4b5b7c9
Eric Arellano <ericarellano@me.com>
2017-12-27 10:26:38 -0700

    remove bootstrap!

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,4 @@
 // react
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/css/bootstrap-theme.css'
 import React from 'react'
 import { render } from 'react-dom'
 import { AppContainer } from 'containers'
