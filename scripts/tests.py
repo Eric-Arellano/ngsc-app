8d272a73463d1f12e5dadfe830711db3dc49adc0
Eric Arellano <ericarellano@me.com>
2018-01-29 10:08:31 -0700

    simplify tests

--- scripts/tests.py
+++ scripts/tests.py
@@ -1,4 +1,3 @@
-from functools import partial
 from typing import Callable, Dict, List
 from unittest import TestCase, skip
 
@@ -9,49 +8,32 @@ import helper
 
 class ParserTester(TestCase):
 
-    def assert_parser_error_thrown(self,
+    def assert_raises_parser_error(self,
                                    command_map: Dict[str, Callable],
-                                   error: Exception,
                                    args: List[str]):
         parser = helper.create_parser(backend.command_map)
         parsed_args = parser.parse_args(args)
-        with self.assertRaises(error):
+        with self.assertRaises(TypeError):
             helper.execute_command(parsed_args, command_map)
 
 
 class TestSingleServerParser(ParserTester):
 
+    def assert_backend_and_frontend_raise_error(self, args: List[str]):
+        self.assert_raises_parser_error(command_map=backend.command_map,
+                                        args=args)
+        self.assert_raises_parser_error(command_map=frontend.command_map,
+                                        args=args)
+
     def test_dependency_management_requires_dependencies(self):
-        # backend
-        backend_test = partial(self.assert_parser_error_thrown,
-                               command_map=backend.command_map,
-                               error=TypeError)
-        backend_test(args=['add'])
-        backend_test(args=['upgrade'])
-        backend_test(args=['remove'])
-        # frontend
-        frontend_test = partial(self.assert_parser_error_thrown,
-                                command_map=frontend.command_map,
-                                error=TypeError)
-        frontend_test(args=['add'])
-        frontend_test(args=['upgrade'])
-        frontend_test(args=['remove'])
+        self.assert_backend_and_frontend_raise_error(args=['add'])
+        self.assert_backend_and_frontend_raise_error(args=['upgrade'])
+        self.assert_backend_and_frontend_raise_error(args=['remove'])
 
     def test_only_dependency_management_allows_variable_arguments(self):
-        # backend
-        backend_test = partial(self.assert_parser_error_thrown,
-                               command_map=backend.command_map,
-                               error=TypeError)
-        backend_test(args=['types', 'bad'])
-        backend_test(args=['run', 'bad'])
-        backend_test(args=['install', 'bad'])
-        # frontend
-        frontend_test = partial(self.assert_parser_error_thrown,
-                                command_map=frontend.command_map,
-                                error=TypeError)
-        frontend_test(args=['types', 'bad'])
-        frontend_test(args=['run', 'bad'])
-        frontend_test(args=['install', 'bad'])
+        self.assert_backend_and_frontend_raise_error(args=['types', 'bad'])
+        self.assert_backend_and_frontend_raise_error(args=['run', 'bad'])
+        self.assert_backend_and_frontend_raise_error(args=['install', 'bad'])
 
 
 class TestRunParser(ParserTester):
