2300580db9106b0ef1727f552b5c61a12a9ce3b4
clair <cblock3@asu.edu>
2018-02-13 18:42:44 -0700

    add nav to top

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,5 +1,6 @@
 // @flow
 import React from 'react'
+import { Link } from 'react-router-dom'
 import logo from './logo.png'
 import s from './Header.module.css'
 
@@ -8,6 +9,12 @@ const Header = () => (
     <a href="/">
       <img src={logo} className={s.logo} alt="logo" />
     </a>
+      <nav>
+          <ul>
+              <li><Link to={'/'}>Home</Link></li>
+              <li><Link to={'/participation'}>Participation</Link></li>
+              </ul>
+      </nav>
     <h1 className={s.title}>NGSC Engagement Requirements</h1>
   </header>
 )
