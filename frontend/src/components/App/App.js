e76343a094d1190490bf7b69a2a41b1567565a69
clair <cblock3@asu.edu>
2018-02-15 20:14:22 -0700

    tab home and tab participation working after login

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,31 +1,33 @@
 // @flow
 import * as React from 'react'
-import { Switch, Route } from 'react-router-dom'
-import { Footer, Header, ParticipationView } from 'components'
-import { LoginViewContainer } from 'containers'
-import type { Student } from 'flow/types'
+import {Switch, Route} from 'react-router-dom'
+import {Footer, Header, ParticipationView} from 'components'
+import {LoginViewContainer} from 'containers'
+import type {Student} from 'flow/types'
 import s from './App.module.css'
 
 type Props = {
-  isLoggedIn: boolean,
-  student: ?Student,
-  resetState: () => void,
-  login: (Student) => void
+    isLoggedIn: boolean,
+    student: ?Student,
+    resetState: () => void,
+    login: (Student) => void
 }
 
 const App = ({isLoggedIn, student, login, resetState}: Props) => (
-  <div className={s.app}>
-    <Header />
-      <Switch>
-          <Route exact path={'/'} component={LoginViewContainer}/>
-          <Route exact path={'/participation'} component={ParticipationView}/>
-      </Switch>
-    {/*<div className={s.body}>*/}
-      {/*{isLoggedIn ? <ParticipationView student={student} resetState={resetState} /> :*/}
+    <div className={s.app}>
+        <Header/>
+        <Switch>
+            <Route exact path='/' render={() => (
+                <LoginViewContainer login={login}/>)}/>
+            <Route exact path='/participation' render={() => (
+                <ParticipationView student={student} resetState={resetState}/>)}/>
+        </Switch>
+        {/*<div className={s.body}>*/}
+        {/*{isLoggedIn ? <ParticipationView student={student} resetState={resetState} /> :*/}
         {/*<LoginViewContainer login={login} />}*/}
-    {/*</div>*/}
-    <Footer />
-  </div>
+        {/*</div>*/}
+        <Footer/>
+    </div>
 )
 
 export default App
