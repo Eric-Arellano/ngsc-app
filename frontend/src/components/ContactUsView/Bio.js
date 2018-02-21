d49fd2e8896c53b72d124ef3d884f4b760436134
Nikki <theninjanikki@nikkis-mbp.mobile.asu.edu>
2018-02-20 19:11:35 -0700

    Added styling for bio

--- frontend/src/components/ContactUsView/Bio.js
+++ frontend/src/components/ContactUsView/Bio.js
@@ -1,5 +1,6 @@
 // @flow
 import React from 'react'
+import s from './Bio.module.css'
 
 type Props = {
     pictureURL: string,
@@ -10,7 +11,7 @@ type Props = {
 
 const Bio = ({pictureURL, position, name, email}: Props) => (
         <div>
-            <img src={pictureURL}/>
+            <img src={pictureURL} className={s.image}/>
             <p>{position}</p>
             <p>{name}</p>
             <p>{email}</p>
