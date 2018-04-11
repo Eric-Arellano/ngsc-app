c4aa59501d57c6e9ae9cb94dac99b11aa35cab77
Eric Arellano <ericarellano@me.com>
2018-04-11 15:48:27 -0700

    add commands to filter & select certain columns of sheets

--- backend/src/sheets_commands/_list_generator.py
+++ backend/src/sheets_commands/_list_generator.py
@@ -1,6 +1,28 @@
 from typing import Any, Dict, List
 
 
+def select_columns(*,
+                   all_cells: List[List[Any]],
+                   target_indexes: List[int]) -> List[List[Any]]:
+    """
+    Given all cells, return only the specified columns. Zero-indexed.
+    """
+    return [[cell for col_index, cell in enumerate(row)
+             if col_index in target_indexes]
+            for row in all_cells]
+
+
+def filter_by_cell(*,
+                   all_cells: List[List[Any]],
+                   target_index: int,
+                   target_value: str) -> List[List[Any]]:
+    """
+    Filter out rows without the target value for specified column. Does not modify rows.
+    """
+    return [row for row in all_cells
+            if len(row) > target_index and row[target_index] == target_value]
+
+
 def update_column(*,
                   updated_values: Dict[str, str],
                   all_cells: List[List[Any]],
@@ -8,7 +30,7 @@ def update_column(*,
                   target_index: int,
                   overwrite: bool) -> List[List[Any]]:
     """
-    Generate an updated list for the entire column, optionally overwriting values.
+    Generate an updated list for the entire column, optionally overwriting values. Zero-indexed.
     """
     if overwrite:
         return [[updated_values.get(row[key_index], row[key_index])]  # if nothing found in dict, keep original value
--- backend/src/sheets_commands/tests/test_list_generator.py
+++ backend/src/sheets_commands/tests/test_list_generator.py
@@ -3,73 +3,163 @@ import pytest
 from backend.src.sheets_commands import _list_generator
 
 
+# --------------------------------------------------------------------
+# Select column(s)
+# --------------------------------------------------------------------
+
+def test_select_column():
+    original_cells = [
+        ['Eric', 'Arellano', '1'],
+        ['Sami', 'Mooney', '2'],
+        ['Diana', 'Chen', '3']
+    ]
+    selected = _list_generator.select_columns(all_cells=original_cells,
+                                              target_indexes=[0, 2])
+    assert selected == [
+        ['Eric', '1'],
+        ['Sami', '2'],
+        ['Diana', '3']
+    ]
+
+
+def test_select_column_missing_cells():
+    original_cells = [
+        ['Eric', 'Arellano', '1'],
+        ['Sami'],
+        ['Diana', '', '3']
+    ]
+    selected = _list_generator.select_columns(all_cells=original_cells,
+                                              target_indexes=[0, 2])
+    assert selected == [
+        ['Eric', '1'],
+        ['Sami'],
+        ['Diana', '3']
+    ]
+
+
+# --------------------------------------------------------------------
+# Filter
+# --------------------------------------------------------------------
+
+def test_filter():
+    original_cells = [
+        ['Eric', '1'],
+        ['Sami', '2'],
+        ['Diana', '2'],
+        ['Raul', '3']
+    ]
+    filtered = _list_generator.filter_by_cell(all_cells=original_cells,
+                                              target_index=1,
+                                              target_value='2')
+    assert filtered == [
+        ['Sami', '2'],
+        ['Diana', '2'],
+    ]
+
+
+def test_filter_missing_cells():
+    original_cells = [
+        ['Eric', '1'],
+        ['Sami'],
+        ['Diana', '2'],
+    ]
+    filtered = _list_generator.filter_by_cell(all_cells=original_cells,
+                                              target_index=1,
+                                              target_value='2')
+    assert filtered == [
+        ['Diana', '2'],
+    ]
+
+
+# --------------------------------------------------------------------
+# Update column
+# --------------------------------------------------------------------
+
 @pytest.fixture
-def dictionary():
-    id_to_new_values = {'ecarell1': '13',
-                        'ecka13': '44'}
+def mock_updated_values():
+    id_to_new_values = {
+        'ecarell1': '13',
+        'ecka13': '44'
+    }
     return id_to_new_values
 
 
-def test_update_column_with_overwrite(dictionary):
-    original_cells = [['ecarell1', ''],
-                      ['ecka13', ''],
-                      ['ecka13', '99']]
-    updated_values = _list_generator.update_column(updated_values=dictionary,
+def test_update_column_with_overwrite(mock_updated_values):
+    original_cells = [
+        ['ecarell1', ''],
+        ['ecka13', ''],
+        ['ecka13', '99']
+    ]
+    updated_values = _list_generator.update_column(updated_values=mock_updated_values,
                                                    all_cells=original_cells,
                                                    key_index=0,
                                                    target_index=1,
                                                    overwrite=True)
-    assert updated_values == [['13'],
-                              ['44'],
-                              ['44']]
+    assert updated_values == [
+        ['13'],
+        ['44'],
+        ['44']
+    ]
 
 
-def test_update_column_without_overwrite(dictionary):
-    original_cells = [['ecarell1', ''],
-                      ['ecka13', ''],
-                      ['ecka13', '99']]
-    updated_values = _list_generator.update_column(updated_values=dictionary,
+def test_update_column_without_overwrite(mock_updated_values):
+    original_cells = [
+        ['ecarell1', ''],
+        ['ecka13', ''],
+        ['ecka13', '99']
+    ]
+    updated_values = _list_generator.update_column(updated_values=mock_updated_values,
                                                    all_cells=original_cells,
                                                    key_index=0,
                                                    target_index=1,
                                                    overwrite=False)
-    assert updated_values == [['13'],
-                              ['44'],
-                              ['99']]
+    assert updated_values == [
+        ['13'],
+        ['44'],
+        ['99']
+    ]
 
 
 @pytest.mark.skip('Not yet implemented')
-def test_update_column_empty_lists(dictionary):
-    original_cells = [['ecarell1'],
-                      [''],
-                      ['', '']]
-    updated_values_with_overwrite = _list_generator.update_column(updated_values=dictionary,
+def test_update_column_empty_lists(mock_updated_values):
+    original_cells = [
+        ['ecarell1'],
+        [''],
+        ['', '']
+    ]
+    updated_values_with_overwrite = _list_generator.update_column(updated_values=mock_updated_values,
                                                                   all_cells=original_cells,
                                                                   key_index=0,
                                                                   target_index=1,
                                                                   overwrite=True)
-    updated_values_without_overwrite = _list_generator.update_column(updated_values=dictionary,
+    updated_values_without_overwrite = _list_generator.update_column(updated_values=mock_updated_values,
                                                                      all_cells=original_cells,
                                                                      key_index=0,
                                                                      target_index=1,
                                                                      overwrite=False)
-    assert updated_values_with_overwrite == updated_values_without_overwrite == [['13'],
-                                                                                 [],
-                                                                                 []]
+    assert updated_values_with_overwrite == updated_values_without_overwrite == [
+        ['13'],
+        [],
+        []
+    ]
 
 
-def test_update_column_additional_data(dictionary):
-    original_cells = [['ecarell1', '', 'e'],
-                      ['ecka13', '', 'e']]
-    updated_values_with_overwrite = _list_generator.update_column(updated_values=dictionary,
+def test_update_column_additional_data(mock_updated_values):
+    original_cells = [
+        ['ecarell1', '', 'e'],
+        ['ecka13', '', 'e']
+    ]
+    updated_values_with_overwrite = _list_generator.update_column(updated_values=mock_updated_values,
                                                                   all_cells=original_cells,
                                                                   key_index=0,
                                                                   target_index=1,
                                                                   overwrite=True)
-    updated_values_without_overwrite = _list_generator.update_column(updated_values=dictionary,
+    updated_values_without_overwrite = _list_generator.update_column(updated_values=mock_updated_values,
                                                                      all_cells=original_cells,
                                                                      key_index=0,
                                                                      target_index=1,
                                                                      overwrite=False)
-    assert updated_values_with_overwrite == updated_values_without_overwrite == [['13'],
-                                                                                 ['44']]
+    assert updated_values_with_overwrite == updated_values_without_overwrite == [
+        ['13'],
+        ['44']
+    ]
