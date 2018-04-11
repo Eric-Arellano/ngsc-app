bd20b822bb11bb82c8986b34341906e6278c1338
Eric Arellano <ericarellano@me.com>
2018-04-11 12:58:51 -0700

    refactor drive's create code

--- backend/src/drive_commands/create.py
+++ backend/src/drive_commands/create.py
@@ -4,25 +4,30 @@ Create an empty file or folder into the specified targets.
 
 from backend.src.google_apis.authentication import build_drive_service
 
+DEFAULT_PARENT_FOLDER_ID = '1QOsOQq3FMYfpXau6v3ubem5wrC97yWwo'  # Scripts/Drive playground
 
-# files created in Scripts/Drive playground
-def file(file_name: str, parent_folder_id: str = '1QOsOQq3FMYfpXau6v3ubem5wrC97yWwo'):
+
+def file(file_name: str,
+         parent_folder_id: str = DEFAULT_PARENT_FOLDER_ID):
     """
     Create an empty file.
     """
     # TODO: Change mime_type to allow multiple different mime types not just 'document'
-    return _create_helper(name=file_name, mime_type='document', parent_folder_id=parent_folder_id)
+    return _create_resource(name=file_name, mime_type='document', parent_folder_id=parent_folder_id)
 
 
-# currently is Scripts/Drive playground
-def folder(folder_name: str, parent_folder_id: str = '1QOsOQq3FMYfpXau6v3ubem5wrC97yWwo'):
+def folder(folder_name: str,
+           parent_folder_id: str = DEFAULT_PARENT_FOLDER_ID):
     """
     Create an empty folder.
     """
-    return _create_helper(name=folder_name, mime_type='folder', parent_folder_id=parent_folder_id)
+    return _create_resource(name=folder_name, mime_type='folder', parent_folder_id=parent_folder_id)
 
 
-def _create_helper(name: str, mime_type: str, parent_folder_id: str = '1QOsOQq3FMYfpXau6v3ubem5wrC97yWwo'):
+def _create_resource(name: str, mime_type: str, parent_folder_id: str):
+    """
+    Create Google Drive file with specific MIME type.
+    """
     service = build_drive_service()
     file_metadata = {
         'name': name,
