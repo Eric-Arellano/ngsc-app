1ef287da907644015efb6082a39da82427883911
Eric Arellano <ericarellano@me.com>
2018-04-15 00:09:40 -0700

    fix crash on missing roster data

--- backend/src/sheets_commands/rows.py
+++ backend/src/sheets_commands/rows.py
@@ -3,9 +3,10 @@ from backend.src.sheets_commands import sheet
 
 def append_blank(*,
                  all_cells: sheet.Grid,
-                 num_rows: int) -> sheet.Grid:
+                 num_rows: int,
+                 num_columns: int) -> sheet.Grid:
     """
     Append blank rows.
     """
-    new_rows = [[''] * len(all_cells[0])] * num_rows
+    new_rows = [[''] * num_columns] * num_rows
     return all_cells + new_rows
--- backend/src/sheets_commands/tests/test_rows.py
+++ backend/src/sheets_commands/tests/test_rows.py
@@ -12,10 +12,23 @@ def test_add_blank():
         ['Sami', '2'],
     ]
     result = rows.append_blank(all_cells=original_cells,
-                               num_rows=2)
+                               num_rows=2,
+                               num_columns=2)
     assert result == [
         ['Eric', '1'],
         ['Sami', '2'],
+        ['', ''],
+        ['', ''],
+    ]
+
+
+def test_add_blank_to_empty():
+    original_cells = [[]]
+    result = rows.append_blank(all_cells=original_cells,
+                               num_rows=2,
+                               num_columns=2)
+    assert result == [
+        [],
         ['', ''],
         ['', ''],
     ]
