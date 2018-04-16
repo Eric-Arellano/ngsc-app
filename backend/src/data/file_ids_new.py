db8fbea446d92eba3e6d5a0ec2cf4e38a10961fb
Eric Arellano <ericarellano@me.com>
2018-04-15 17:54:58 -0700

    save temporary files so that I can deploy

---
+++ backend/src/data/file_ids_new.py
@@ -0,0 +1,54 @@
+committee_attendance = {'Admin': '1oO3z8BA1Zk8O1170Dq9HYAohNHFX0K5sZKbyX2s2Dp0',
+ 'Ambassadors': '1CGsClxXxls6nVF5CYyRF2cQBzDJZeVCItZiElHBaeQ0',
+ 'Civil-Mil': '1xYgsvvvL1V6n6kfc6thvJVrLZJsq3EPLJaIIyK_qtI8',
+ 'Communications': '1NaGHJFISLDuyh6JLiEL9U03ulmGS6tkq6tsH0VnumXU',
+ 'Events': '1z4xIzdGr9qQPRzRCXBQzl4WWzfXgjbc2QxGKBtTGDhA',
+ 'Mentorship': '1daC4syuU4Uwiixf6ifJ4uebt0BmdiFW4UmNcq18QRCE',
+ 'Service': '1I1pvykC874xfTVg-BaRA0CfyhmSVP01G2DpHxooMSq0',
+ 'Social': '1jm8EF9UXHA-d60pzmK1Y3LdBfDErM-WAiHha7aDZvF0',
+ 'Training': '1vVddSpnePJ-Ia6GHjiVcOu3r8QVUZ56oYAlQ0qNKTnQ',
+ 'Transfers': '1XE2mF3PMRPzaqqLVgpgdg2Cj79sDHlqPoY0tiTQ74WI'}
+
+mission_team_attendance = {1: '1zKQ9qpXvrdWlY01LJoYuda_0-9Iy-JSp4AeojXa0LbY',
+ 2: '1ue68g6EIdwgFKXQWgwb4f0HELVcB-dQQPSDPnryZ9Ks',
+ 3: '17JPuPDEKiQHxs2C5_7MMk_S9eDr7Gwej7dfvGeAZYAI',
+ 4: '1CpMeG4_1rMFgGNWlpaHuHDIp2eextjkEo28aMzQTvjM',
+ 5: '1wjOdFhtcAfqn2aDTbIkTTqzEIjN0JeUzhywG6A8neZg',
+ 6: '1j2ENtxn5mhRJXqOgEeQ4uITtfB-Q4TA92OJi-0nYWtY',
+ 7: '1CtxVYCG9vMM6z6j1SPNYG8xTeYIzUzOEc4rek9LiJes',
+ 8: '12d6qziKxKWY50hotY4LNWszXuuMjrjl5KVuA2dQPpOk',
+ 9: '1dQiF3_Wy14ms2SdmGWqYe54NtJru3gnYpKsQAH8-i24',
+ 10: '1RA2jBJ6N5QEWOZvrA_ZP_aL8EFptt2glZSlx3KHh6dE',
+ 11: '1NCZtR6YlD2AoqGQQUjvTuV5cFfAvTs9h0ee_Hs7R75o',
+ 12: '1WHA6WTsLy8iVzBx3AK2OxMQC6mP8QQOWfJ4W6ZbmvCo',
+ 13: '1nYfyQ_5CLEVC9m0OaJfw7swD6VgavWU_xE9BejH2zpY',
+ 14: '1WAgIVf8J-GVivGtvhxoEqzUWO5kjHA6-KiGpw2iJz7U',
+ 15: '1fy4X8teXk7YbzIzjY8PWDWutJeH-Z7a-xOdxnoiXq50',
+ 16: '1QOesy-5Qe6XYumY9RhbyS21MydI_TQR7VW5vUnWiSrs',
+ 17: '1MXyKlXar27zzCFb1jsqqjDumCXiNXiFG5rVMK94PHOo',
+ 18: '1_FH7_o7wW1op2FdtqVFlZmmiZS3WftJFwYUVs4MDzuw',
+ 19: '1pzIMoxJXGKV3yn3y021ps3jCZj2QD1wQ5VUDdUBuE98',
+ 20: '1U8p75NO8MxuIOYBsSdaDmy-Fynn_HRSPRqVokhWokzk',
+ 21: '1nX5HgYDZSGm_EL1gc-k_oyrKSjxEWjULfCB3qxi0Klc',
+ 22: '17mbbIgfkamlxyxvn7svh1P3ABi7Bon9E4wyC6BtqfLc',
+ 23: '1UJescN1gkfrjOjnJuW-JhWb1OCfMSvEEn6IVFW58YgI',
+ 24: '1TsoVDkSOnRbEYLJ1YldyHGMDOmd5RMhUPKNggDqNvIg',
+ 25: '1BtYDcBuPU-vz5k8tQ87qEl_qY72iE9NWa1c-DC66-p4',
+ 26: '1x4Y-ifkEu1wByK37qHMJiI99JZZdRs6y0Mm5VzMilNA',
+ 27: '1aAd9NSUMAozDlzXdHmgM0OAnxMfBmreDVhhVOkOdLFw',
+ 28: '1MXmALWNDbhDqVjqBt3Q2k21bMHoYPnC8moNk7n127oo',
+ 29: '1UBYkDPleRFBD7iSBCBaWVm7HxR-l3o3iMmgJTUhyPM0',
+ 30: '16UZrmwbvtGm77pz0BuhJTgPZg_7Hnq1rXTBW1AmBxdY'}
+
+master = '1h63tzPpYjXD17KHEA37GglMpAQ0JChz99jBrC_RwCho'
+
+schedule = '1oagV2WVfWPRVUSZKckMfvmON09WtxzNYkfjSKmlQenk'
+
+participation = {'all_students': '1RiPrSIh2zZB5yooAFnuEN4Jqm2mF3w2fo0n1lgfPxJ4',
+ 'engagement': '1lpC0oFajMDsgFYMMagJlVL7Wkw2kYo8MKyzaGjeDN0w',
+ 'no_shows': '17i8VYN3dQUQ6lzCIKZKiJjYb9DjKVVm21Bz_ZBx7wms'}
+
+templates = {'event_proposal': '1r0fco52uysIBqZgV0PEioy6yYviV3Bvj6xxfi4twV24',
+ 'initial_meeting': '1hsp1sAEqTCXm_3zZFl2l8-htsTXJpEHTSVjnqDN5ubQ',
+ 'ols_cancel_rsvp': '1mrG1zhjUeDNf5ZQMJOVOnIRysq-7bRtVJD2mjUamPtw',
+ 'rsvp': '1HTVVqIQbDi0MTB6nZ2GUXWv4bTN40zB4xwFxMwkWCxY'}
---
+++ backend/src/data/folder_ids_new.py
@@ -0,0 +1,78 @@
+ngsc_root = '0B4Ek7KKF4u8EMFJNN0duXzcxZXM'
+
+drive_playground = '1QOsOQq3FMYfpXau6v3ubem5wrC97yWwo'
+
+semester_root = '1NLbbdeJtVNRYQu_Fctuz_GzRGmHhnrEU'
+
+templates = '1G5XtRr_qNdS-mfvmDFfZajnXe6vH5Gny'
+
+all_students_root = '1x3E1SbZeWwDSOToTPbW4INmn9Mz2OJ9J'
+
+all_students = {'on_leadership': '1s-_NEGfIG4Od_PL-qSeFw5IDVlzNvC3P',
+ 'participation': '1s4sSl2e-L9QrnJl-5WLqTtC8XeXKZD8-',
+ 'summit': '1b33UeWGwZViIpMrhwHayr-hY0XwrwwVI'}
+
+leadership_root = '1_Y8a9eHdCdmzGOcbuja0FDvi1p3iHFZB'
+
+leadership = {'staff_briefings': '1wCEYDvt_nJvFK8oYKTeu7Js3yXS86wWH'}
+
+sections_root = '1WqXqSn2Dxa4nYewn_sGhsYJ1s-HaWE-A'
+
+sections = {1: '1iNt1qLpQB-tgLPxI1UsDb_4dlq-i26Jh',
+ 2: '1wqBGdGOCweHA9z2TDnaypWDCnzQ6OOrG',
+ 3: '1Eb1DUYIm7JbIkxGQW3K68rb9kToKZl61',
+ 4: '1RbC0NRmYiDNlbfS1ZweN6JSu5xqesAOl',
+ 5: '1CCdC3gY0LbjGrhVmzWOt4962g-eqY7SZ',
+ 6: '1edVbAvkwXjSPwoTH0hkB-GBjYl-FhoLC',
+ 7: '1FFQHZRBJGzezOBgfrUYhqWqXMGi6ht5L',
+ 8: '1YDc4ZCZEUWnVUWNWBDd4JTxzEGhzh6tG',
+ 9: '1JPg7QTK_f-87VZ6mvLVBongHx0A5ZSL4',
+ 10: '1uCzBMZjb1BE3-5G08O8tifelR5L_Q9SF'}
+
+mission_teams = {1: '1pEaJExXfRmszU5lCc2XbYRduXHVvBajJ',
+ 2: '1jQdHd3669SyJyDV202_z_olwdnHzfW6H',
+ 3: '1EMlFWSxkw6jf-__XLVc_Begk2a_5A2qp',
+ 4: '1qGgW50so3jDCSEf9nluM_ZTAOlnbURmi',
+ 5: '1KoRW1gTtDRHyWnqwwTIwazmV-YO8-VUN',
+ 6: '1Iab9B2vwxMMspT1j3CbQgbHf5vzDQ1Ik',
+ 7: '1VLp3S4Ri5CuAcKqE8UoweFRPlketemHa',
+ 8: '1fPBOT4w2tFlu2_PqzcM2HlcYsqyVKXXH',
+ 9: '1ZEZFG322b6sdmjTozC7VDB_PpmkV8MFI',
+ 10: '1OoVkgYrsGXZVmiL_X81bkZw1KrTWa-PS',
+ 11: '1phdtOZ1BOxrdLYAHUfX34bx5e6ry6uWP',
+ 12: '16J_bsqxmS5Lv38IBONKcICC8646vRI-R',
+ 13: '1e_pDbFBdPWDyIZnAJnzx8YHuB1aJpVYu',
+ 14: '1BsjwH1elsSNYwHlpAtjYgxm1_bSsbEp_',
+ 15: '1qDdp_7G5DtD0eCdPzcoUe4HKHfJK3oCv',
+ 16: '1Ib4NH5Cop8jl417R93PSvFOhUf7XeyKN',
+ 17: '151KfcgHb4YpiNTucjtOl8AYIK7nCLmTH',
+ 18: '1_teVWgxHFgLJaTlDhbjvnlwcsbllH8_s',
+ 19: '1n-eLdGEJLLToN15RfTOFBD9qrjUWw-7p',
+ 20: '1rTADXF5ADJOTkku9vidLdEElKMc6baPY',
+ 21: '1gqQ0TtV-Xu7kjVfxJ-WKF49YQaPwb0-W',
+ 22: '1fR2xIBxPrN2RNL0m2sPMDNsLLr5fnb1d',
+ 23: '18CncD7V0Lwt012Ojca89sRPlBuRSYApz',
+ 24: '1FjFg-nF8Lycg_YbuxaIC776NcPdO4TQB',
+ 25: '1ALN41FhFQxNd80PpPQizjFnrUGmcL1nD',
+ 26: '1IP5xhg-zzSxi1qBopdIhYcBysDam4ouP',
+ 27: '1uBJCCKRLS7YsnZ6qOO4CFOEjtEiruePl',
+ 28: '1xGqmapFVhVhSIsJC6N2zzVXTIAYbHdmD',
+ 29: '1-x__pmuFfAUNG-7aA6NpqV5KcXCpYpw2',
+ 30: '150GhGxk5-O11clp8c5OIxbXauQf9Sby0'}
+
+committees_root = '1FQHgJlYaVkiUkE7xnSpZY_HhfCMEdDkn'
+
+committee_leads = {'culture': '1Mf7wpsP1bSprHdoTi6b95fsNZnXx3ls-',
+ 'education': '1X_aNmsc4zqa9-KVI3cQ7lFs4bkbM6FRU',
+ 'engagement': '1645sxKx5d96pTowX6atmFMgtZSuJgvpC'}
+
+committees = {'Admin': '1Wwccp7gLqp0w0p681WFh2eswy248bhVi',
+ 'Ambassadors': '1wIIzydqcjv8PDJxnnM3OFxBUlBy7xtZB',
+ 'Civil-Mil': '1pePZ4LtzQRpDXUftJ7ji7higLP9swgU5',
+ 'Communications': '1-nk-MCLp_9v3l3VLSK5RIevIp9QsYFrl',
+ 'Events': '1XCTh9XNoTGOXAXCcuu7RTpV4JNeGLkh_',
+ 'Mentorship': '1Vqia0EbOPAfg25qx8R-BL8HDzBRnBtCG',
+ 'Service': '1rtaulHtdcIJOSInyKvlW3jPw4iIA1OU7',
+ 'Social': '1Suwy0xiedBSSolHQTto7gyDyKBXTTZZT',
+ 'Training': '1gD2112why0_X35uwnUPaAmwdCJyk-6Qh',
+ 'Transfers': '1m_UpIfX_4TidD_g0oIIpSf_XNOtkiBkb'}
