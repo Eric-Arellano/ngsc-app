17aea5c4ce9adcc150145bbc7790d1b88c7bf562
Eric Arellano <ericarellano@me.com>
2018-04-20 12:26:19 -0700

    add protected range

--- backend/src/sheets_commands/validation.py
+++ backend/src/sheets_commands/validation.py
@@ -27,6 +27,30 @@ def dropdown_options(*,
     sheet.batch_update(spreadsheet_id, [request])
 
 
+def protected_range(*,
+                    spreadsheet_id: sheet.ID,
+                    editor_emails: List[str] = None,
+                    warning_only: bool = False,
+                    description: str = None,
+                    row_start_index: int,
+                    row_end_index: int,
+                    column_start_index: int,
+                    column_end_index: int,
+                    tab_id: str = '0') -> None:
+    """
+    Protect range so only provided users can edit.
+    """
+    request = protected_range_request(editor_emails=editor_emails,
+                                      warning_only=warning_only,
+                                      description=description,
+                                      row_start_index=row_start_index,
+                                      row_end_index=row_end_index,
+                                      column_start_index=column_start_index,
+                                      column_end_index=column_end_index,
+                                      tab_id=tab_id)
+    sheet.batch_update(spreadsheet_id, [request])
+
+
 # ---------------------------------------------------------------------
 # Generate requests (allows delaying execution)
 # ---------------------------------------------------------------------
@@ -59,3 +83,39 @@ def dropdown_options_request(*,
                 'showCustomUi': True
             },
         }}
+
+
+def protected_range_request(*,
+                            editor_emails: List[str] = None,
+                            warning_only: bool = False,
+                            description: str = None,
+                            row_start_index: int,
+                            row_end_index: int,
+                            column_start_index: int,
+                            column_end_index: int,
+                            tab_id: str = '0') -> sheet.BatchRequest:
+    """
+    Protect range so only provided users can edit.
+    """
+    request = {
+        'addProtectedRange': {
+            'protectedRange': {
+                'range': {
+                    'sheetId': tab_id,
+                    'startRowIndex': row_start_index,
+                    'endRowIndex': row_end_index,
+                    'startColumnIndex': column_start_index,
+                    'endColumnIndex': column_end_index,
+                },
+                'warningOnly': warning_only,
+                'requestingUserCanEdit': True
+            }
+        }
+    }
+    if description:
+        request['addProtectedRange']['protectedRange']['description'] = description
+    if editor_emails:
+        request['addProtectedRange']['protectedRange']['editors'] = {
+            'users': editor_emails
+        }
+    return request
