5b0de801a64d9cc0ed6c79da2015bd2b8ef1dc8a
Eric Arellano <ericarellano@me.com>
2018-04-21 14:57:15 -0700

    setup leadership & new_semester data folder

---
+++ backend/src/data/leadership.py
@@ -0,0 +1,70 @@
+chief_of_staff = ''
+
+staff = ['']
+
+section_leads = {
+    '1': '',
+    '2': '',
+    '3': '',
+    '4': '',
+    '5': '',
+    '6': '',
+    '7': '',
+    '8': '',
+    '9': '',
+    '10': ''
+}
+
+mission_team_leaders = {
+    '1': '',
+    '2': '',
+    '3': '',
+    '4': '',
+    '5': '',
+    '6': '',
+    '7': '',
+    '8': '',
+    '9': '',
+    '10': '',
+    '11': '',
+    '12': '',
+    '13': '',
+    '14': '',
+    '15': '',
+    '16': '',
+    '17': '',
+    '18': '',
+    '19': '',
+    '20': '',
+    '21': '',
+    '22': '',
+    '23': '',
+    '24': '',
+    '25': '',
+    '26': '',
+    '27': '',
+    '28': '',
+    '29': '',
+    '30': ''
+}
+
+committee_leads = {
+    'Culture': '',
+    'Education': '',
+    'Engagement': '',
+}
+
+committee_chairs = {
+    'Admin': '',
+    'Ambassadors': '',
+    'Civil-Mil': '',
+    'Communication': '',
+    'Leadership': '',
+    'Mentoring': '',
+    'Service': '',
+    'Events': '',
+    'Social': '',
+    'Training': ''
+}
+
+drive_root_access = [chief_of_staff] + staff + [committee_chairs['Admin']]
