ed24a76d6e0256275d318a4ed6902319d9a7b7cf
Eric Arellano <ericarellano@me.com>
2018-08-27 14:15:50 -0700

    Store old roster data in legacy tabs (#35)

--- backend/src/admin/new_semester_scripts/rebuild_rosters.py
+++ backend/src/admin/new_semester_scripts/rebuild_rosters.py
@@ -17,22 +17,29 @@ sys.path.append(str(current_file_path.parents[4]))
 import textwrap
 from typing import NamedTuple
 
+from googleapiclient import discovery
+
 from scripts.utils import command_line
 from backend.src.google_apis import sheets_api
 from backend.src.drive_commands import generate_link
 from backend.src.data.new_semester import new_file_ids
+from backend.src.admin import _environment_chooser
 from backend.src.admin.new_semester_scripts import setup_semester
+from backend.src.sheets_commands import tab, sheet
 
 
 def main() -> None:
     # check preconditions
     setup_semester.check_new_ids_different()
-    confirm_rebuild()
+    override = ask_if_override()
     check_master_updated()
     # choose target
     roster_targets = choose_rosters()
     # execute
     sheets_service = sheets_api.build_service()
+    if not override:
+        create_legacy_tabs(include_committees=roster_targets.committees,
+                           include_mission_teams=roster_targets.mission_teams)
     setup_semester.prepare_all_rosters(include_committees=roster_targets.committees,
                                        include_mission_teams=roster_targets.mission_teams,
                                        add_colors=False,
@@ -40,17 +47,20 @@ def main() -> None:
     setup_semester.update_participation_id_list(sheets_service=sheets_service)
 
 
-def confirm_rebuild() -> None:
+# ---------------------------------------------------------------
+# Options & Instructions
+# --------------------------------------------------------------
+
+def ask_if_override() -> bool:
     """
     Make sure user really does want to overwrite all rosters.
     """
-    confirmation = command_line.ask_yes_no(
+    return command_line.ask_yes_no(
             question=textwrap.dedent('''\
-                    This script will overwrite ALL data on the new semester\'s rosters.
-                    Are you sure you want to continue?'''),
-            default='no')
-    if confirmation is False:
-        raise SystemExit('Canceling. The rosters will be kept as is.')
+                    Would you like to override the old values? Otherwise it will create a new tab on each roster 
+                    saving the old values.
+                    (You should override in general, unless the semester has already started. Remember to delete the 
+                    legacy tabs a week after this change.)'''))
 
 
 def check_master_updated() -> None:
@@ -91,6 +101,36 @@ def choose_rosters() -> RosterTargets:
         return RosterTargets(committees=False, mission_teams=True)
     else:
         return RosterTargets(committees=False, mission_teams=False)
+
+
+# ---------------------------------------------------------------
+# Save old data
+# --------------------------------------------------------------
+
+@command_line.log(start_message='Copying old rosters into legacy tabs.',
+                  end_message='Old rosters copied over.\n')
+def create_legacy_tabs(*,
+                       include_committees: bool,
+                       include_mission_teams: bool,
+                       sheets_service: discovery.Resource = None) -> None:
+    legacy_tab_id = 999_999
+
+    def create(spreadsheet_id: str) -> None:
+        tab.new(spreadsheet_id=spreadsheet_id,
+                tab_name='OLD DATA',
+                tab_id=legacy_tab_id,
+                sheets_service=sheets_service)
+        grid = sheet.get_values(spreadsheet_id=spreadsheet_id,
+                                range_='C:Z',
+                                sheets_service=sheets_service)
+        sheet.update_values(spreadsheet_id=spreadsheet_id,
+                            range_="'OLD DATA'!A:Z",
+                            grid=grid,
+                            sheets_service=sheets_service)
+    if include_committees:
+        _environment_chooser.operate_on_all_committee_attendance(create)
+    if include_mission_teams:
+        _environment_chooser.operate_on_all_mission_team_attendance(create)
 
 
 if __name__ == '__main__':
--- backend/src/sheets_commands/tab.py
+++ backend/src/sheets_commands/tab.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from googleapiclient import discovery
 
 from backend.src.sheets_commands import sheet
@@ -6,6 +8,20 @@ from backend.src.sheets_commands import sheet
 # ---------------------------------------------------------------------
 # Commands (immediate execution)
 # ---------------------------------------------------------------------
+
+def new(*,
+        spreadsheet_id: sheet.ID,
+        tab_name: str,
+        tab_id: Optional[int] = None,
+        sheets_service: discovery.Resource = None) -> None:
+    """
+    Create a new tab with given name.
+    """
+    request = new_request(tab_name=tab_name, tab_id=tab_id)
+    sheet.batch_update(spreadsheet_id,
+                       requests=[request],
+                       sheets_service=sheets_service)
+
 
 def rename(*,
            spreadsheet_id: sheet.ID,
@@ -25,6 +41,24 @@ def rename(*,
 # ---------------------------------------------------------------------
 # Generate requests (allows delaying execution)
 # ---------------------------------------------------------------------
+
+def new_request(*,
+                tab_name: str,
+                tab_id: Optional[int] = None) -> sheet.BatchRequest:
+    """
+    Create a new tab with given name.
+    """
+    request = {
+        'addSheet': {
+            'properties': {
+                'title': tab_name
+            },
+        }
+    }
+    if tab_id is not None:
+        request['addSheet']['properties']['sheetId'] = tab_id
+    return request
+
 
 def rename_request(*,
                    tab_name: str,
